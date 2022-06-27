321c4396c8bc2b292c70dfefc59fb223eb850c1b
tr7zw <tr7zw@live.de>
2022-06-28 00:47:21 +0200

    Extact GraphUtil into own project

---
+++ GU-Fabric/src/main/java/dev/tr7zw/graphutil/GraphUtilMod.java
@@ -0,0 +1,17 @@
+package dev.tr7zw.graphutil;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GraphUtilMod extends GraphUtilModBase implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        super.onInitialize();
+    }
+
+    @Override
+    public void initModloader() {
+
+    }
+
+}
