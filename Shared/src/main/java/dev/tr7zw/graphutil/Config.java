81fe09e32605e2330619ed62dfacb218fbbd6cc1
tr7zw <tr7zw@live.de>
2022-09-11 23:02:02 +0200

    Add clipping option, replace icon

--- Shared/src/main/java/dev/tr7zw/graphutil/Config.java
+++ Shared/src/main/java/dev/tr7zw/graphutil/Config.java
@@ -3,7 +3,8 @@ package dev.tr7zw.graphutil;
 public class Config {
 
     public int configVersion = 1;
-    public boolean vanillaScale = false;
+    public boolean vanillaScale = true;
+    public boolean preventClipping = true;
     public boolean alwaysShowGraph = false;
     
 }
