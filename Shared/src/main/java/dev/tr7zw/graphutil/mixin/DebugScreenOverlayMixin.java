f16d87302d3f87422b0b7d47a3f8a20d07bbe7c3
tr7zw <tr7zw@live.de>
2023-06-09 17:36:13 +0200

    Update to 1.20

--- Shared/src/main/java/dev/tr7zw/graphutil/mixin/DebugScreenOverlayMixin.java
+++ Shared/src/main/java/dev/tr7zw/graphutil/mixin/DebugScreenOverlayMixin.java
@@ -15,7 +15,6 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.BufferBuilder;
 import com.mojang.blaze3d.vertex.BufferUploader;
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
-import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.Tesselator;
 import com.mojang.blaze3d.vertex.VertexFormat;
 import com.mojang.math.Transformation;
@@ -23,14 +22,15 @@ import com.mojang.math.Transformation;
 import dev.tr7zw.graphutil.GraphUtilModBase;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
-import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.DebugScreenOverlay;
 import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.util.FrameTimer;
 import net.minecraft.util.Mth;
 
 @Mixin(DebugScreenOverlay.class)
-public abstract class DebugScreenOverlayMixin extends GuiComponent {
+public abstract class DebugScreenOverlayMixin {
 
     @Shadow
     @Final
@@ -42,18 +42,18 @@ public abstract class DebugScreenOverlayMixin extends GuiComponent {
     private DecimalFormat df = new DecimalFormat("0.00");
 
     @Inject(method = "render", at = @At("HEAD"))
-    public void render(PoseStack poseStack, CallbackInfo ci) {
-        if(GraphUtilModBase.instance.config.alwaysShowGraph) {
+    public void render(GuiGraphics guiGraphics, CallbackInfo ci) {
+        if (GraphUtilModBase.instance.config.alwaysShowGraph) {
             this.minecraft.options.renderFpsChart = true;
         }
     }
-    
+
     @Inject(method = "drawChart", at = @At("HEAD"), cancellable = true)
-    private void drawChart(PoseStack poseStack, FrameTimer frameTimer, int i, int j, boolean fpsGraph,
+    private void drawChart(GuiGraphics guiGraphics, FrameTimer frameTimer, int i, int j, boolean fpsGraph,
             CallbackInfo ci) {
         if (!fpsGraph) {
-            if(!GraphUtilModBase.instance.config.hideTPS) {
-                drawServerChart(poseStack, frameTimer, i, j);
+            if (!GraphUtilModBase.instance.config.hideTPS) {
+                drawServerChart(guiGraphics, frameTimer, i, j);
             }
             ci.cancel();
             return;
@@ -82,8 +82,8 @@ public abstract class DebugScreenOverlayMixin extends GuiComponent {
 
         }
         t = this.minecraft.getWindow().getGuiScaledHeight();
-        fill(poseStack, i, t - 60, i + p, t, -1873784752);
-        
+        guiGraphics.fill(RenderType.guiOverlay(), i, t - 60, i + p, t, -1873784752);
+
         boolean vanillaScale = GraphUtilModBase.instance.config.vanillaScale;
 
         RenderSystem.setShader(GameRenderer::getPositionColorShader);
@@ -100,7 +100,7 @@ public abstract class DebugScreenOverlayMixin extends GuiComponent {
         while (m != l) {
             float ms = ls[m] / 1000000f;
             ms -= minMs;
-            int v = vanillaScale ? frameTimer.scaleSampleTo(ls[m], 30, 60) : (int) (ms/scaled *60f);
+            int v = vanillaScale ? frameTimer.scaleSampleTo(ls[m], 30, 60) : (int) (ms / scaled * 60f);
             int w = 100;
             int x = getSampleColor(Mth.clamp(v, 0, w), 0, w / 2, w);
 
@@ -121,46 +121,47 @@ public abstract class DebugScreenOverlayMixin extends GuiComponent {
         }
         BufferUploader.drawWithShader(bufferBuilder.end());
         RenderSystem.disableBlend();
-        
-        if (vanillaScale) {
-            fill(poseStack, i + 1, t - 30 + 1, i + 14, t - 30 + 10, -1873784752);
-            this.font.draw(poseStack, "60 FPS", (i + 2), (t - 30 + 2), 14737632);
-            hLine(poseStack, i, i + p - 1, t - 30, -1);
-            fill(poseStack, i + 1, t - 60 + 1, i + 14, t - 60 + 10, -1873784752);
-            this.font.draw(poseStack, "30 FPS", (i + 2), (t - 60 + 2), 14737632);
-            hLine(poseStack, i, i + p - 1, t - 60, -1);
-        }
-        
-        hLine(poseStack, i, i + p - 1, t - 1, -1);
-        vLine(poseStack, i, t - 60, t, -1);
-        vLine(poseStack, i + p - 1, t - 60, t, -1);
 
-        if(!vanillaScale) {
-            hLine(poseStack, i, i + p - 1,  t - 1 - (int)(((totalMs / p)-minMs)/scaled*60f), -16711681);
-            this.font.drawShadow(poseStack, "avg", (i + p), t - 6 - (int)(((totalMs / p)-minMs)/scaled*60f), 14737632);
+        if (vanillaScale) {
+            guiGraphics.fill(RenderType.guiOverlay(), i + 1, t - 30 + 1, i + 14, t - 30 + 10, -1873784752);
+            guiGraphics.drawString(this.font, "60 FPS", (i + 2), (t - 30 + 2), 14737632);
+            guiGraphics.hLine(RenderType.guiOverlay(), i, i + p - 1, t - 30, -1);
+            guiGraphics.fill(RenderType.guiOverlay(), i + 1, t - 60 + 1, i + 14, t - 60 + 10, -1873784752);
+            guiGraphics.drawString(this.font, "30 FPS", (i + 2), (t - 60 + 2), 14737632);
+            guiGraphics.hLine(RenderType.guiOverlay(), i, i + p - 1, t - 60, -1);
+        }
+
+        guiGraphics.hLine(RenderType.guiOverlay(), i, i + p - 1, t - 1, -1);
+        guiGraphics.vLine(RenderType.guiOverlay(), i, t - 60, t, -1);
+        guiGraphics.vLine(RenderType.guiOverlay(), i + p - 1, t - 60, t, -1);
+
+        if (!vanillaScale) {
+            guiGraphics.hLine(RenderType.guiOverlay(), i, i + p - 1, t - 1 - (int) (((totalMs / p) - minMs) / scaled * 60f), -16711681);
+            guiGraphics.drawString(this.font, "avg", (i + p), t - 6 - (int) (((totalMs / p) - minMs) / scaled * 60f),
+                    14737632);
         } else {
             int avgValue = frameTimer.scaleSampleTo((long) (totalMs * 1000000 / p), 30, 60);
-            if(!(preventClipping && avgValue > 60)) {
-                hLine(poseStack, i, i + p - 1,  t - 1 - avgValue, -16711681);
-                this.font.drawShadow(poseStack, "avg", (i + p), t - 6 - avgValue, 14737632);
+            if (!(preventClipping && avgValue > 60)) {
+                guiGraphics.hLine(RenderType.guiOverlay(), i, i + p - 1, t - 1 - avgValue, -16711681);
+                guiGraphics.drawString(this.font, "avg", (i + p), t - 6 - avgValue, 14737632);
             }
         }
-        
+
         String string = "" + df.format(minMs) + " ms min";
         String string2 = "" + df.format(totalMs / p) + " ms avg";
         String string3 = "" + df.format(maxMs) + " ms max";
         Objects.requireNonNull(this.font);
-        this.font.drawShadow(poseStack, string, (i + 2), (t - 60 - 9), 14737632);
+        guiGraphics.drawString(this.font, string, (i + 2), (t - 60 - 9), 14737632);
         Objects.requireNonNull(this.font);
-        this.font.drawShadow(poseStack, string2, (i + p / 2 - this.font.width(string2) / 2), (t - 60 - 9), 14737632);
+        guiGraphics.drawString(this.font, string2, (i + p / 2 - this.font.width(string2) / 2), (t - 60 - 9), 14737632);
         Objects.requireNonNull(this.font);
-        this.font.drawShadow(poseStack, string3, (i + p - this.font.width(string3)), (t - 60 - 9), 14737632);
+        guiGraphics.drawString(this.font, string3, (i + p - this.font.width(string3)), (t - 60 - 9), 14737632);
 
         RenderSystem.enableDepthTest();
         ci.cancel();
     }
-    
-    private void drawServerChart(PoseStack poseStack, FrameTimer frameTimer, int i, int j) {
+
+    private void drawServerChart(GuiGraphics guiGraphics, FrameTimer frameTimer, int i, int j) {
         RenderSystem.disableDepthTest();
         int k = frameTimer.getLogStart();
         int l = frameTimer.getLogEnd();
@@ -177,23 +178,24 @@ public abstract class DebugScreenOverlayMixin extends GuiComponent {
         int t;
         for (t = 0; t < p; t++) {
             float u = ls[frameTimer.wrapIndex(m + t)] / 1000000f;
-            if(u == 0)continue;
+            if (u == 0)
+                continue;
             minMs = Math.min(minMs, u);
             maxMs = Math.max(maxMs, u);
             totalMs += u;
             count++;
         }
         t = this.minecraft.getWindow().getGuiScaledHeight();
-        fill(poseStack, i, t - 60, i + p, t, -1873784752);
+        guiGraphics.fill(RenderType.guiOverlay(), i, t - 60, i + p, t, -1873784752);
         RenderSystem.setShader(GameRenderer::getPositionColorShader);
         BufferBuilder bufferBuilder = Tesselator.getInstance().getBuilder();
         RenderSystem.enableBlend();
         RenderSystem.defaultBlendFunc();
         bufferBuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_COLOR);
         Matrix4f matrix4f = Transformation.identity().getMatrix();
-        
+
         boolean preventClipping = GraphUtilModBase.instance.config.preventClipping;
-        
+
         while (m != l) {
             int i1 = frameTimer.scaleSampleTo(ls[m], 60, 20);
             int w = 60;
@@ -213,29 +215,29 @@ public abstract class DebugScreenOverlayMixin extends GuiComponent {
         BufferUploader.drawWithShader(bufferBuilder.end());
         RenderSystem.disableBlend();
 
-        fill(poseStack, i + 1, t - 60 + 1, i + 14, t - 60 + 10, -1873784752);
-        this.font.draw(poseStack, "20 TPS", (i + 2), (t - 60 + 2), 14737632);
-        hLine(poseStack, i, i + p - 1, t - 60, -1);
+        guiGraphics.fill(RenderType.guiOverlay(), i + 1, t - 60 + 1, i + 14, t - 60 + 10, -1873784752);
+        guiGraphics.drawString(this.font, "20 TPS", (i + 2), (t - 60 + 2), 14737632);
+        guiGraphics.hLine(RenderType.guiOverlay(), i, i + p - 1, t - 60, -1);
 
-        hLine(poseStack, i, i + p - 1, t - 1, -1);
-        vLine(poseStack, i, t - 60, t, -1);
-        vLine(poseStack, i + p - 1, t - 60, t, -1);
+        guiGraphics.hLine(RenderType.guiOverlay(), i, i + p - 1, t - 1, -1);
+        guiGraphics.vLine(RenderType.guiOverlay(), i, t - 60, t, -1);
+        guiGraphics.vLine(RenderType.guiOverlay(), i + p - 1, t - 60, t, -1);
 
         int avgValue = frameTimer.scaleSampleTo((long) (totalMs * 1000000 / count), 60, 20);
-        if(!(preventClipping && avgValue > 60)) {
-            hLine(poseStack, i, i + p - 1,  t - 1 - avgValue, -16711681);
-            this.font.drawShadow(poseStack, "avg", (i -18), t - 6 - avgValue, 14737632);
+        if (!(preventClipping && avgValue > 60)) {
+            guiGraphics.hLine(RenderType.guiOverlay(), i, i + p - 1, t - 1 - avgValue, -16711681);
+            guiGraphics.drawString(this.font, "avg", (i - 18), t - 6 - avgValue, 14737632);
         }
-        
+
         String string = "" + df.format(minMs) + " ms min";
         String string2 = "" + df.format(totalMs / p) + " ms avg";
         String string3 = "" + df.format(maxMs) + " ms max";
         Objects.requireNonNull(this.font);
-        this.font.drawShadow(poseStack, string, (i + 2), (t - 60 - 9), 14737632);
+        guiGraphics.drawString(this.font, string, (i + 2), (t - 60 - 9), 14737632);
         Objects.requireNonNull(this.font);
-        this.font.drawShadow(poseStack, string2, (i + p / 2 - this.font.width(string2) / 2), (t - 60 - 9), 14737632);
+        guiGraphics.drawString(this.font, string2, (i + p / 2 - this.font.width(string2) / 2), (t - 60 - 9), 14737632);
         Objects.requireNonNull(this.font);
-        this.font.drawShadow(poseStack, string3, (i + p - this.font.width(string3)), (t - 60 - 9), 14737632);
+        guiGraphics.drawString(this.font, string3, (i + p - this.font.width(string3)), (t - 60 - 9), 14737632);
         RenderSystem.enableDepthTest();
     }
 
